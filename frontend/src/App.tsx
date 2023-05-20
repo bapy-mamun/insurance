5756c8b5e07839f1c8e5f9020be645f6902d4312
mamun.abdullah <mamun_abdullah@PC20003.local>
2023-05-20 16:35:53 +0900

    adding router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Product() {
+  return <h2>This is a page for product with ID: </h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products/1">First Product</Link>
+            </li>
+            <li>
+              <Link to="/products/2">Second Product</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" Component={Index} />
+          <Route path="/products/:id" Component={Product} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
