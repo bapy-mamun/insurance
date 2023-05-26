1bad9e71dac1d4f21776b86c904e4f340dd4727b
mamun.abdullah <mamun_abdullah@PC20003.local>
2023-05-26 22:49:33 +0900

    creating form button handlers

--- packages/frontend/src/pages/buy-insurance/index.tsx
+++ packages/frontend/src/pages/buy-insurance/index.tsx
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 import Form from "../../components/molecules/form";
+import { InsuranceProduct } from "../../utils/types";
 
 const BuyInsurance: React.FC = () => {
+  const [formStep, setFormStep] = React.useState<"step1" | "step2">("step1");
+  const [insuranceData, setInsuranceData] = useState<InsuranceProduct[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "http://localhost:8080/api/insuranceProducts"
+      );
+      const data = await response.json();
+      console.log(data);
+      setInsuranceData(data.insuranceProducts);
+    };
+    fetchData();
+  }, []);
+
+  const handleSubmit = () => {
+    console.log('submit');
+  };
+
+  const handleNextPrevClick = () => {
+    if (formStep === "step1") {
+      setFormStep("step2");
+    } else {
+      setFormStep("step1");
+    }
+  };
+
   return (
     <div className="p-buyInsurance">
-      <div className="p-buyInsurance__form">
-        <Form />
+      {formStep === "step1" ? (
+        <div className="p-buyInsurance__form">
+          <Form />
+        </div>
+      ) : (
+        <div className="p-buyInsurance__payementForm">
+          <p>payement</p>
+        </div>
+      )}
+      <div className="p-buyInsurance__payementForm">
+        <button onClick={handleNextPrevClick}>
+          {formStep === "step1" ? "Next" : "Previous"}
+        </button>
+        {formStep === "step2" ? <button onClick={handleSubmit}>Buy</button> : null}
       </div>
     </div>
-  )
+  );
 };
 
 export default BuyInsurance;
